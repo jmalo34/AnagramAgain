1436a30834963991a3f8a17d1623d3a5e89c3c2b
Jenna Maloney <jmalo@uw.edu>
2015-11-25 23:11:28 -0800

    make first 4 tests pass, display correctly with Twig

--- app/app.php
+++ app/app.php
@@ -1,6 +1,8 @@
 <?php
     require_once __DIR__.'/../vendor/autoload.php';
     require_once __DIR__.'/../src/Anagram.php';
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
 
     $app = new Silex\Application();
     $app['debug'] = TRUE;
@@ -13,9 +15,12 @@
 
     $app->get('anagramno', function() use($app)
     {
-        $my_Anagram = new Anagram($_GET['original']);
-        $word = $my_Anagram->check($_GET['word']);
-        return $app['twig']->render('anagramno.html.twig', array('words' => $word));
+
+        $my_Anagram = new Anagram($_GET['a_word']);
+        $yes_anagrams = $my_Anagram->check($_GET['the_word']);
+        return $app['twig']->render('anagramno.html.twig', array('words' => $yes_anagrams));
+    var_dump($yes_anagrams);
+    var_dump($words);
     });
 
     return $app;
--- src/Anagram.php
+++ src/Anagram.php
@@ -1,32 +1,47 @@
 <?php
     class Anagram
     {
-        private $original;
+        private $a_word;
 
-        function __construct($original)
+        function __construct($a_word)
         {
-            $this->original = $original;
+            $this->a_word = $a_word;
         }
 
-        function check($word)
+        function setAword($new_a_word)
         {
-            $word = str_split(strtolower(preg_replace('/[^a-zA-Z]/', '', $word)));
+            $this->a_word = $new_a_word;
+        }
 
-            $remixed = str_split(strtolower($this->original));
+        function getAword()
+        {
+            return $this->a_word;
+        }
 
-            sort($word);
-            sort($remixed);
+        function check($the_word)
+        {
+            $word_list = explode(' ', $this->a_word);
 
-            if ((implode($word)) == (implode($remixed)))
+            $remixed_the_word = str_split(strtolower(preg_replace('/[^a-zA-Z]/', '', ($the_word))));
+
+            sort($remixed_the_word);
+
+            $yes_anagrams = array();
+
+            foreach ($word_list as $word)
             {
-                $YorN = 'YESSIR';
-            }
-            else
-            {
-                $YorN = 'NOPE';
+                // $word = new Anagram($word);
+                $single_morph = str_split(strtolower(preg_replace('/[^a-zA-Z]/', '', $word)));
+
+                sort($single_morph);
+
+                if ((implode($single_morph)) == (implode($remixed_the_word)))
+                {
+                    array_push($yes_anagrams, new Anagram($word));
+                }
             }
 
-            return $YorN;
+            return $yes_anagrams;
         }
     }
  ?>
